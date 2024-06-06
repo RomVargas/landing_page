bcaaeb74c6eaeb6324f87416132a7ccf71db4036
Roman <romvargas81@gmail.com>
2024-06-06 15:37:37 -0600

    se agrega audio y se corrigen errores de estilos

---
+++ js/contact.js
@@ -0,0 +1,275 @@
+var audio = function() {
+   audio = document.getElementById("myAudio");
+};
+window.onload = audio;
+
+var $loadingMessage = $('#loadingH1');
+    
+   setTimeout(function() {
+         clearInterval(loading);
+         $loadingMessage.hide();
+      }, 1500);
+   
+   var initProgram = setTimeout(function(){
+       var greeting = "--- GATHERING RESUME INFO ---";
+       var message = "RUNNING  RESUME PROTOCAL (CRP) FOR ROMAN VARGAS";
+       var $identityDiv = $("#identity-results");
+       var $name = "NAME: ROMAN VARGAS";
+       var $alias = "KNOWN ALIAS: @ROMVARGAS81"
+       var $occupation  = "OCCUPATION: FULL-STACK DEVELOPER / DEVOPS";
+       var $frontEnd  = "FRONT END DEVELOPMENT CORE: HTML5 | CSS3 | JAVASCRIPT ";
+         var $JSFrameworks = "JAVASCRIPT FRAMEWORKS: ANGULAR";
+         var $JSLibs = "JAVASCRIPT LIBRARIES: JQUERY | REACT";
+         var $CSSFrameworks = "CSS FRAMEWORKS: BOOTSTRAP | TAILWIND";
+         var $CSSPre = "CSS PREPROCESSORS: SASS | LESS ";
+         var $frontEndAnimation = "FRONT END ANIMATION: CANVAS | HTML5 | SVG | THREEjs";
+       var $cmsDevelopment = "WORDPRESS DEVELOPMENT: WORDPRESS";
+         var $wordpress = "WORDPRESS: CUSTOM THEMING | PLUGINS";
+       var $design = "DESIGN CORE: ADOBE SUITE";
+         var $webDesign = "WEB DESIGN: INDESIGN CC | MUSE CC";
+         var $branding = "ILLUSTRATIONS & BRANDING: ILLUSTRATOR CC | PHOTOSHOP CC";
+         var $designAnimation = "ANIMATION DESIGN: AFTER EFFECTS CC | FLASH CC";
+         
+      function initIdentityResults(i){
+         audio.play();
+          $("#message").addClass("sign cursor").text(message.substring(0, i));
+            if(i < message.length){
+                setTimeout(function(){
+                    initIdentityResults(i + 1);
+                }, 6);   
+            }else{
+               $('#message').removeClass("cursor");
+               var loadingResume = function(){
+                     $("#loadingMessage2").show()
+                     var dotAlpha = document.getElementById("alpha-loading");
+                     var loadingAlpha = setInterval(function() {
+                      if(dotAlpha.innerHTML.length == 6) {
+                          dotAlpha.innerHTML = "";
+                      } else {
+                          dotAlpha.innerHTML += ".";
+                          setTimeout(function() {
+                              clearInterval(loadingAlpha);
+                              $("#loadingMessage2").hide();
+                           }, 2000);
+                      }
+                  }, 50);  // Dot Speed
+               }
+               loadingResume();
+               function initName(i){
+                  $("#name").addClass("sign cursor").text($name.substring(0, i));
+                     if(i < $name.length){
+                        setTimeout(function(){
+                           initName(i + 1);   
+                        }, 
+                        6);
+                     }else{
+                        $("#name").removeClass("cursor");
+                        setTimeout(function() {
+                           initAlias(0);
+                        }, 100);      
+                     }
+               }
+               setTimeout(function() {
+                  initName(0);
+               }, 250);
+            }
+            function initAlias(i){
+                  $("#alias").addClass("sign cursor").text($alias.substring(0, i));
+                     if(i < $alias.length){
+                        setTimeout(function(){
+                           initAlias(i + 1);   
+                        }, 6);
+                     }else{
+                        $("#alias").removeClass("cursor");
+                        setTimeout(function() {
+                           initOccupation(0);
+                        }, 100);
+                     }
+            }
+            function initOccupation(i){
+               $("#occupation").addClass("sign cursor").text($occupation.substring(0, i));
+                     if(i < $occupation.length){
+                        setTimeout(function(){
+                           initOccupation(i + 1);   
+                        }, 6);
+                     }else{
+                        $("#occupation").removeClass("cursor");
+                        setTimeout(function() {
+                           initFrontEnd(0);
+                        }, 100);
+                     }
+            }
+            function initFrontEnd(i){
+               $('#front-end-span').addClass('fa fa-wrench');
+               $('#front-end').addClass("cursor").text($frontEnd.substring(0, i));
+                  if(i < $frontEnd.length){
+                     setTimeout(function(){
+                        initFrontEnd(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#front-end').removeClass("cursor");
+                        setTimeout(function() {
+                           initJSFrameworks(0);
+                        }, 100);
+                  }
+            }
+            function initJSFrameworks(i){
+               $('#js-frameworks').addClass("sign cursor").text($JSFrameworks.substring(0, i));
+                  if(i < $JSFrameworks.length){
+                     setTimeout(function(){
+                         initJSFrameworks(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#js-frameworks').removeClass("cursor");
+                        setTimeout(function() {
+                           initJSLibs(0);
+                        }, 100);
+                  }
+            }
+            function initJSLibs(i){
+               $('#js-libs').addClass("sign cursor").text($JSLibs.substring(0, i));
+                  if(i < $JSLibs.length){
+                     setTimeout(function(){
+                         initJSLibs(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#js-libs').removeClass("cursor");
+                        setTimeout(function() {
+                           initCSSFrameworks(0);
+                        }, 100);
+                  }
+            }
+            function initCSSFrameworks(i){
+               $('#css-frameworks').addClass("sign cursor").text($CSSFrameworks.substring(0, i));
+                  if(i < $CSSFrameworks.length){
+                     setTimeout(function(){
+                         initCSSFrameworks(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#css-frameworks').removeClass("cursor");
+                     setTimeout(function() {
+                           initCSSPre(0);
+                        }, 100);
+                  }
+            }
+             function initCSSPre(i){
+               $('#css-pre').addClass("sign cursor").text($CSSPre.substring(0, i));
+                  if(i < $CSSPre.length){
+                     setTimeout(function(){
+                         initCSSPre(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#css-pre').removeClass("cursor");
+                     setTimeout(function() {
+                           initFrontEndAnimation(0);
+                        }, 100);
+                  }
+            }
+            function initFrontEndAnimation(i){
+               $('#front-end-animation').addClass("sign cursor").text($frontEndAnimation.substring(0, i));
+                  if(i < $frontEndAnimation.length){
+                     setTimeout(function(){
+                         initFrontEndAnimation(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#front-end-animation').removeClass("cursor");
+                     setTimeout(function() {
+                           initCMSDevelopment(0);
+                        }, 100);
+                  }
+            }
+            function initCMSDevelopment(i){
+               $('#cms-span').addClass("fa fa-wordpress")
+               $('#CMS-development').addClass("cursor").text($cmsDevelopment.substring(0, i));
+                  if(i < $cmsDevelopment.length){
+                     setTimeout(function(){
+                         initCMSDevelopment(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#CMS-development').removeClass("cursor");
+                     setTimeout(function() {
+                           initWordpress(0);
+                        }, 100);
+                  }
+            }
+            function initWordpress(i){
+               $('#wordpress').addClass("sign cursor").text($wordpress.substring(0, i));
+                  if(i < $wordpress.length){
+                     setTimeout(function(){
+                         initWordpress(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#wordpress').removeClass("cursor");
+                     setTimeout(function() {
+                           initDesign(0);
+                        }, 100);
+                  }
+            }
+             function initDesign(i){
+               $('#design-span').addClass('fa fa-paint-brush');
+               $('#design').addClass("cursor").text($design.substring(0, i));
+                  if(i < $design.length){
+                     setTimeout(function(){
+                         initDesign(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#design').removeClass("cursor");
+                     setTimeout(function() {
+                           initWebDesign(0);
+                        }, 180);
+                  }
+            }
+            function initWebDesign(i){
+               $('#web-design').addClass("sign cursor").text($webDesign.substring(0, i));
+                  if(i < $webDesign.length){
+                     setTimeout(function(){
+                         initWebDesign(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#web-design').removeClass("cursor");
+                     setTimeout(function() {
+                           initBranding(0);
+                        }, 100);
+                  }
+            }
+            function initBranding(i){
+               $('#branding').addClass("sign cursor").text($branding.substring(0, i));
+                  if(i < $branding.length){
+                     setTimeout(function(){
+                         initBranding(i + 1);  
+                     }, 8);
+                  }else{
+                     $('#branding').removeClass("cursor");
+                     setTimeout(function() {
+                           initDesignAnimation(0);
+                        }, 100);
+                  }
+            }
+            function initDesignAnimation(i){
+               $('#design-animation').addClass("sign cursor").text($designAnimation.substring(0, i));
+                  if(i < $designAnimation.length){
+                     setTimeout(function(){
+                         initDesignAnimation(i + 1);  
+                     }, 6);
+                  }else{
+                     $('#design-animation').removeClass("cursor");
+                  }
+            }
+         }
+         
+      function initProgramAlpha(i){
+          $("#greeting").addClass("cursor").text(greeting.substring(0, i));
+            if(i < greeting.length){
+                setTimeout(function(){
+                    initProgramAlpha(i + 1);
+                }, 6);   
+            }else{
+               $("#greeting").removeClass("cursor");
+               initIdentityResults(0);
+            }
+         }
+         initProgramAlpha(0)
+   
+   }, 100);
+
+
